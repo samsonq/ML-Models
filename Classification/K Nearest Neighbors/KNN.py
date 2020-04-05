485c198ec5946fa5516fe08448cea40e04caa601
Samson Qian <samsonqian@gmail.com>
2020-04-05 11:26:33 -0700

    fixed knn bug

--- Classification/K Nearest Neighbors/KNN.py
+++ Classification/K Nearest Neighbors/KNN.py
@@ -7,27 +7,33 @@ import numpy as np
 class KNN:
     """
     A K Nearest Neighbors model to make classifications on inputted data by computing the 'k'
-    nearest data points to a new data point and taking the majority class as the vote. Calculates the
-    Euclidean distance between 2 points. Option to standardize data before fitting to improve performance.
+    nearest data points to a new data point and taking the majority class as the vote. Calculates
+    the Euclidean distance between points to find closest neighbors.
     """
-    def __init__(self, neighbors=1):
+    def __init__(self, neighbors=1, standardize=False):
         """
         Initialize data of model and number of neighbors to find.
         :param neighbors: number of nearest neighbors
+        :param standardize: standardize data to scale
         """
         self.neighbors = neighbors  # number of neighbors (k)
+        self.standardize = standardize
+        if self.standardize:
+            self.mean = 0
+            self.std = 0
         self.data = None
         self.labels = None
 
-    def fit(self, X, y, standardize=False):
+    def fit(self, X, y):
         """
         Fits training data to model for making predictions.
         :param X: array of training features
         :param y: array of training labels
-        :param standardize: standardize data
         """
-        if standardize:
-            X = (X - np.mean(X, axis=0))/np.std(X, axis=0)  # standardize each column
+        if self.standardize:
+            self.mean = np.mean(X, axis=0)
+            self.std = np.std(X, axis=0)
+            X = (X - self.mean)/self.std  # standardize each column
         self.data = X
         self.labels = y
 
@@ -37,6 +43,8 @@ class KNN:
         :param X: array of test features
         :return: predicted labels of test set
         """
+        if self.standardize:
+            X = (X - self.mean)/self.std
         return np.array([self._predict(x) for x in X])
 
     def _predict(self, x):
@@ -45,13 +53,13 @@ class KNN:
         :param x: one data point
         :return: predicted label of data point
         """
-        distances = [self.euclidean_distance(x, i) for i in self.data]
+        distances = [self._euclidean_distance(x, i) for i in self.data]
         nearest = np.argsort(distances)[:self.neighbors]
         neighbors = [self.labels[i] for i in nearest]
         return max(neighbors, key=neighbors.count)
 
     @staticmethod
-    def euclidean_distance(x1, x2):
+    def _euclidean_distance(x1, x2):
         """
         Calculates the Euclidean distance between 2 data points.
         :param x1: first data point
